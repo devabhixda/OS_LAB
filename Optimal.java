185c9ac0eb426d242fa3ef549fc1ce0e96ea7d8b
Abhi Jain <39195567+devabhixda@users.noreply.github.com>
2020-02-23 11:30:02 +0530

    Improve frame filling logic in Optimal algorithm

--- Optimal.java
+++ Optimal.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Optimal{
-	public static void main(String args[]){
+    public static void main(String args[]) {
 		List<Integer> list = new ArrayList<Integer>();
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
@@ -9,29 +9,41 @@ class Optimal{
 			list.add(scan.nextInt());
 		}
 		temp=n;
-		int i=0;
 		int size=0;
 		int pagemisses=0;
 		List<Integer> al = new ArrayList<Integer>(4);
 		while(temp-->0){
-			if(!al.contains(list.get(i)) && size<4){
-				al.add(list.get(i++));
+			if(!al.contains(list.get(0)) && size<4){
+				al.add(list.get(0));
 				size++;
 				pagemisses++;
-			} else if(!al.contains(list.get(i))){
+				list.remove(0);
+			} else if(!al.contains(list.get(0))){
 				int min=0;
-				for(int j=0;j<4;j++){
-					if(list.indexOf(al.get(j))<min){
+				int j;
+				for(j=0;j<4;j++){
+				    if(!list.contains(al.get(j))){
+				        al.remove(j);
+				        min=-1;
+				        break;
+				    }
+					if(list.indexOf(al.get(j))>min){
 						min=list.indexOf(al.get(j));
 					}
-					
 				}
-				System.out.println(al);
-				System.out.println(min);
-				al.add(min,list.get(i++));
-				System.out.println(al);
-			}
+				if(min>-1){
+				    int indx = al.indexOf(list.get(min));
+				    al.remove(indx);
+				    al.add(indx,list.get(0));
+				    list.remove(0);
+				} else {
+				    al.add(j, list.get(0));
+				    list.remove(0);
+				}
+				pagemisses++;
+			} else
+			    list.remove(0);
 		}
-		System.out.println(al);
+		System.out.println(pagemisses);
 	}
 }
