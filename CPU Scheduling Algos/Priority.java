e88b3f4e003d4aa5ec8dddaad0bca1154524260c
Abhi Jain <devabhixda@gmail.com>
2020-03-03 10:21:57 +0530

    Priority full revamp
    Signed-off-by: Abhi Jain <devabhixda@gmail.com>

--- CPU Scheduling Algos/Priority.java
+++ CPU Scheduling Algos/Priority.java
@@ -1,101 +1,91 @@
 import java.util.*;
-public class Priority { 
 
-    public static List<String> sortByAt(Map<String, Integer[]> hm) { 
-        List<Map.Entry<String, Integer[]> > list = 
-               new LinkedList<Map.Entry<String, Integer[]> >(hm.entrySet()); 
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer[]> >() { 
-            public int compare(Map.Entry<String, Integer[]> o1,  
-                               Map.Entry<String, Integer[]> o2) 
-            { 
-                return (o1.getValue()[0]).compareTo(o2.getValue()[0]); 
-            } 
-        }); 
-        List<String> temp = new ArrayList<String>(); 
-        for (Map.Entry<String, Integer[]> aa : list) { 
-            temp.add(aa.getKey()); 
-        } 
-        return temp; 
-    } 
-    
-    public static List<String> sortByPriority(Map<String, Integer[]> hm) { 
-        List<Map.Entry<String, Integer[]> > list = 
-               new LinkedList<Map.Entry<String, Integer[]> >(hm.entrySet()); 
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer[]> >() { 
-            public int compare(Map.Entry<String, Integer[]> o1, Map.Entry<String, Integer[]> o2) 
-            {
-                return (o1.getValue()[2]).compareTo(o2.getValue()[2]); 
-            } 
-        }); 
-        List<String> temp = new ArrayList<String>(); 
-        for (Map.Entry<String, Integer[]> aa : list) { 
-            temp.add(aa.getKey()); 
-        } 
-        return temp; 
-    } 
-    
-    static void peruaf(Map<String, Integer[]> hm, List<String> at){
-    	List<Integer> arrival = new ArrayList<>();
-    	List<Integer> burst = new ArrayList<>();
-    	for(int i=0;i<at.size();i++){
-    		arrival.add(hm.get(at.get(i))[0]);
-    		burst.add(hm.get(at.get(i))[1]);	
-    	}
-    	System.out.println(arrival);
-    	System.out.println(burst);
-    }
-    
-	static void findWaitingTime(String pid[], int bt[], int quantum) { 
-		int rem_bt[] = new int[pid.length]; 
-		int count=0;
-		for (int i = 0 ; i < pid.length ; i++) 
-            rem_bt[i] = bt[i]; 
-        int t = 0;
-		while(true) { 
-			boolean done = true; 
-			for (int i = 0 ; i < pid.length; i++) { 
-				if (rem_bt[i] > 0) { 
-					done = false;
-					if (rem_bt[i] > quantum) { 
-						t += quantum; 
-						rem_bt[i] -= quantum; 
-					} 
-					else { 
-						t = t + rem_bt[i]; 
-						rem_bt[i] = 0; 
-					} 
-                } else {
-                    done = true;
-                    break;
-                }
-                count+=rem_bt[i];
-                System.out.println(pid[i]+ " "+rem_bt[i]+" "+t); 
-			} 
-			if (done==true) 
-			    break; 
-		} 
-		System.out.println("Total waiting time= "+count);
-		System.out.println("Average waiting time= "+(float)count/pid.length);
-	} 
-	
-	public static void main(String[] args) { 
+public class Priority {
+
+    static class process {
+        String processName;
+        int arrivalTime;
+        int burstTime;
+        int burstTime1;
+        int completion;
+        int waitingTime;
+        int startTime;
+        double ratio;
+        double num;
+        int turnaroundtime;
+        int priorityvalue;
         Scanner scan = new Scanner(System.in);
-        int n=scan.nextInt();
-        Map<String, Integer[]> map = new HashMap<>();
-        System.out.println("Pid AT BT Priority");
-        for(int i=0;i<n;i++){
-            String pid = scan.next();
-            Integer[] arr = new Integer[3];
-            arr[0] = scan.nextInt();
-            arr[1] = scan.nextInt();
-            arr[2] = scan.nextInt();
-            map.put(pid, arr); 
+        process() {
+            processName = scan.next();
+            arrivalTime = scan.nextInt();
+            burstTime = scan.nextInt();
+            priorityvalue = scan.nextInt();
+            System.out.println();
         }
-        List<String> at = new ArrayList<String>(sortByAt(map));
-        List<String> priority = new ArrayList<String>(sortByPriority(map));
-        System.out.println(at);
-        System.out.println(priority);
-		//findWaitingTime(pid, bt, quantum); 
-		peruaf(map, at);
-	} 
-} 
+    }
+    public static void main(String[] args) {
+
+        int aa, b;
+        int no_Process, total_Burst_Time = 0;
+        int totalwaitingTime = 0, totalturnaroundtime = 0;;
+        int count = 0;
+        double large = 0, averagewaitingTime = 0, averageturnaroundtime = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("No. of processes");
+        no_Process = scan.nextInt();
+        System.out.println("PID AT BT Priority");
+        ArrayList<process> process = new ArrayList<>();
+        for (int i = 0; i < no_Process; i++) {
+            process.add(new process());
+        }
+        for (int i = 0; i < no_Process; i++) {
+            total_Burst_Time += process.get(i).burstTime;
+            process.get(i).burstTime1 = process.get(i).burstTime;
+            process.get(i).completion = 0;
+        }
+        while (count <= total_Burst_Time) {
+            b = 0;
+            large = 0;
+            for (aa = 0; aa < no_Process; aa++) {
+                if (process.get(aa).arrivalTime > count||process.get(aa).burstTime<=0) {
+                    b++;
+                }
+            }
+
+            if (b == no_Process) {
+                count++;
+                continue;
+            }
+
+            for (int i = 0; i < no_Process; i++) {
+                if (process.get(i).arrivalTime > count || process.get(i).burstTime <= 0) {
+                    continue;
+                } else {
+                    if (process.get(i).priorityvalue > large) {
+                        large = process.get(i).priorityvalue;
+                    }
+                } 
+            }
+            for (int i = 0; i < no_Process; i++) {
+                if (large == process.get(i).priorityvalue) {
+                    process.get(i).burstTime = process.get(i).burstTime - 1;
+                    count++;
+                     System.out.println(" this one  "+process.get(i).processName);
+                    if (process.get(i).burstTime <= 0) {
+                        process.get(i).completion = count;
+                        //System.out.println(" this one  "+process.get(i).processName+""+process.get(i).completion);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < no_Process; i++) {
+            totalwaitingTime = +totalwaitingTime + process.get(i).waitingTime;
+            totalturnaroundtime = totalturnaroundtime + process.get(i).turnaroundtime;
+        }
+        averagewaitingTime = totalwaitingTime / no_Process;
+        averageturnaroundtime = totalturnaroundtime / no_Process;
+        System.out.println("Average waiting time = " + averagewaitingTime);
+        System.out.println("Average turnaround  time = " + averageturnaroundtime);
+    }
+}
